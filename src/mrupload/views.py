770e9cf8a4b2f9ceaff17d8c6928959937018c5d
Justin Mitchel <jmitchel3@gmail.com>
2016-03-16 13:03:14 -0700

    Updated for requesting files from form

--- src/mrupload/views.py
+++ src/mrupload/views.py
@@ -12,12 +12,13 @@ from .forms import VideoModelForm
 @login_required
 def formset_upload_view(request):
     VideoModelFormset = modelformset_factory(Video, form=VideoModelForm)
-    formset = VideoModelFormset(request.POST or None)
+    formset = VideoModelFormset(request.POST or None, request.FILES or None)
     if formset.is_valid():
         #formset.save(commit=False)
         for form in formset:
             obj = form.save(commit=False)
             if form.cleaned_data:
+                obj.user = request.user
                 obj.save()
     context = {
         "formset": formset
