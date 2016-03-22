dc7179817b2463d7a73433a99791fd5c3544290f
Ed Patrick Tan <pat.keeps.looking.up@gmail.com>
2016-03-22 16:53:37 +0800

    Added signal whenever a new Video is created

--- src/mrupload/models.py
+++ src/mrupload/models.py
@@ -2,18 +2,19 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils.encoding import smart_text
-
-# Create your models here.
 
 User =  settings.AUTH_USER_MODEL
 
 
 def mr_upload_location(instance, filename):
     return "{user}/videos/{file}".format(
-            user=instance.user.username, 
+            user=instance.user.username,
             file=filename
             )
+
 
 class Video(models.Model):
     user            = models.ForeignKey(User)
@@ -24,3 +25,9 @@ class Video(models.Model):
 
     def __unicode__(self):
         return smart_text(self.title)
+
+
+@receiver(post_save, sender=Video, dispatch_uid='new_video')
+def new_video_created(sender, instance, created, **kwargs):
+    if created:
+        print 'URL of new Video is: %s' % instance.file.url
