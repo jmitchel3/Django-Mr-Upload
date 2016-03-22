ffe5f6a903cf22d3eaf8de12f82d592f8ecd30f6
Ed Patrick Tan <pat.keeps.looking.up@gmail.com>
2016-03-22 17:05:31 +0800

    Show link for uploaded file to user when editing a Video

--- src/mrupload/forms.py
+++ src/mrupload/forms.py
@@ -14,6 +14,16 @@ class VideoForm(forms.ModelForm):
             'description',
         ]
 
+    def __init__(self, *args, **kwargs):
+        super(VideoForm, self).__init__(*args, **kwargs)
+        self.fields['file'] = forms.FileField(widget=forms.FileInput)
+
+        if self.instance.pk:
+            current_file_link = 'Currently: <a href="{file_url}">{file_name}</a>'
+            self.fields['file'].help_text = current_file_link.format(
+                file_url=self.instance.get_file_secure_url(),
+                file_name=self.instance.file.name,
+            )
 
 class MultipleFileForm(FileFormMixin, forms.Form):
     video_files = MultipleUploadedFileField(label='Drop files to upload.')
