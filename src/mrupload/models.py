fa2082ae743c088fb6b9fd2eba781fea73ad5b23
Ed Patrick Tan <pat.keeps.looking.up@gmail.com>
2016-03-22 17:04:59 +0800

    Added custom method for getting secure S3 URL of uploaded file

--- src/mrupload/models.py
+++ src/mrupload/models.py
@@ -6,6 +6,8 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.encoding import smart_text
 
+import boto
+
 User =  settings.AUTH_USER_MODEL
 
 
@@ -25,6 +27,22 @@ class Video(models.Model):
 
     def __unicode__(self):
         return smart_text(self.title)
+
+    def get_file_secure_url(self):
+        """
+        Returns a secure URL for the `file` that was uploaded to S3
+        """
+        conn = boto.s3.connect_to_region(
+            settings.S3DIRECT_REGION,
+            aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            is_secure=True
+        )
+        bucket = conn.get_bucket(settings.AWS_STORAGE_BUCKET_NAME)
+        key = bucket.get_key(self.file.name)
+
+        if key:
+            return key.generate_url(expires_in=settings.AWS_FILE_EXPIRE, method='GET')
 
 
 @receiver(post_save, sender=Video, dispatch_uid='new_video')
